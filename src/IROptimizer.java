6b911eabde0843216a746732acebcba5d04d590c
VicerExciser <acondict11@gmail.com>
2020-01-23 04:38:17 -0500

    added skeleton for our project 1 optimizer

---
+++ src/IROptimizer.java
@@ -0,0 +1,23 @@
+import ir.*;
+import ir.datatype.IRArrayType;
+import ir.datatype.IRIntType;
+import ir.datatype.IRType;
+import ir.operand.IRConstantOperand;
+import ir.operand.IROperand;
+import ir.operand.IRVariableOperand;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IROptimizer {
+
+    ////  TODO  ////
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("PROJECT 1 OPTIMIZER NOT YET IMPLEMENTED");
+    }
+
+    ////  TODO  ////
+
+}
